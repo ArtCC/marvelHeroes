55f4307fa424af2bc9d10dd28c6deba100bba458
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2020-06-15 18:28:59 +0200

    Add function for get credential with timestamp, private and public key.

--- MarvelHeroes/Source/Models/Repositories/CharacterRepositoryImpl.swift
+++ MarvelHeroes/Source/Models/Repositories/CharacterRepositoryImpl.swift
@@ -9,4 +9,23 @@
 import UIKit
 
 class CharacterRepositoryImpl: CharacterRepository {
+    
+    fileprivate struct Constants {
+        
+        static let basePath = "https://gateway.marvel.com/v1/public/characters?"
+        static let privateKey = "a68a7e61345d6a714370727b3dde8e44a4b27ea2"
+        static let publicKey = "4002ee1976a96a15c89cce80085ae2e0"
+        static let limit = 50
+    }
+}
+
+private extension CharacterRepositoryImpl {
+    
+    /// Get parameters for general request
+    /// - Returns: timestamp, apikey and hash (timestamp + private key + public key in MD5 hash)
+    func getCredentials() -> String {
+        let ts = String(Date().timeIntervalSince1970)
+        let hash = MD5.MD5Hex(data: MD5.MD5(string: ts + Constants.privateKey + Constants.publicKey)).lowercased()
+        return "&ts=\(ts)&apikey=\(Constants.publicKey)&hash=\(hash)"
+    }
 }
