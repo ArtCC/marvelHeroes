19cb10b2acc2dda33ce1306d76a414ec640fcba7
Arturo Carretero Calvo <a.carretero.calvo@accenture.com>
2020-06-15 16:25:38 +0200

    Initial Commit

---
+++ MarvelHeroes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by Arturo Carretero Calvo on 15/06/2020.
+//  Copyright © 2020 Arturo Carretero Calvo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
